e4e41cfd984a3e92ab9fb264f18f53c4d4f681a8
Ashley Wong <ashwong04@gmail.com>
2018-09-04 19:54:45 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each {|k, v| puts "Happy Birthday #{k}! You are now #{v} years old!"
+  birthday_kids.each {|k, v| puts "Happy Birthday #{k}! You are now #{v} years old!"}
 end
 
 
